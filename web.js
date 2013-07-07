03685bd37c70b0d15a6aed947199b4142ff87e9c
rem-max <max.vfk@gmail.com>
2013-07-07 10:45:43 +0000

    01

--- web.js
+++ web.js
@@ -2,15 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var fs = require('fs');
-//  buf = new Buffer(256);
-//  buf = fs.readFileSync('index.html');
+var fs = require('fs');
+  buf = new Buffer(256);
+  buf = fs.readFileSync('index.html');
 
-//var str = buf.toString();
+var str = buf.toString();
 
 //var sss='olololololol';
 app.get('/', function(request, response) {
-  response.send("hell");
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
