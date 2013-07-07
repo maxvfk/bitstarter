0b85589633fff3c77b310228212aaed734c2ff24
rem-max <max.vfk@gmail.com>
2013-07-07 10:23:56 +0000

    23f=

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+  buf = new Buffer(256);
+  buf = fs.readFileSync('123.HTML');
+
+var str = buf.toString()
+
+var sss='olololololol'
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(sss);
 });
 
 var port = process.env.PORT || 5000;
