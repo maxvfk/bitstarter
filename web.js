20f3a686fd1a479720f1ecd0590cbef253969386
Maxim Osipov <max.vfk@gmail.com>
2013-07-19 23:47:19 +0400

    index added

--- web.js
+++ web.js
@@ -3,17 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-  buf = new Buffer(256);
-  buf = fs.readFileSync('index.html');
 
-var str = buf.toString();
+
+var str = fs.readFileSync('index.html').toString();
 
 //var sss='olololololol';
 app.get('/', function(request, response) {
   response.send(str);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
